ae4327a2c7339a6ccc6f37e480f72cca10ed290b
Rostislav Bobrovsky <rostislav.bobrovsky@gmail.com>
2020-07-08 11:06:48 +0300

    feat: add init command

---
+++ commands/init.js
@@ -0,0 +1,38 @@
+import fs from 'fs'
+import path from 'path'
+
+import { paths } from '../constants'
+
+if (!fs.existsSync(path.join(paths.app.directory, 'package.json'))) {
+    console.error(`Please, run mould init within your project directory`)
+    process.exit(1)
+}
+
+if (fs.existsSync(paths.app.mouldDirectory)) {
+    console.warn(
+        `You already have ${path.basename(paths.app.mouldDirectory)} ` +
+            `initialized at ${paths.app.directory}`
+    )
+} else {
+    fs.mkdirSync(paths.app.mouldDirectory)
+
+    console.log(
+        `Created ${path.basename(paths.app.mouldDirectory)} ` +
+            `directory at ${paths.app.directory}`
+    )
+}
+
+if (!fs.existsSync(paths.app.resolvers)) {
+    fs.writeFileSync(paths.app.resolvers, 'export default {}')
+
+    console.log(
+        `Created ${path.basename(paths.app.resolvers)} ` +
+            `at ${paths.app.mouldDirectory}`
+    )
+}
+
+console.log(
+    '\nYou could begin by typing:\n\n' +
+        '  npx mould dev\n\n' +
+        'Or you could add mould dev to your package.json scripts\n'
+)
