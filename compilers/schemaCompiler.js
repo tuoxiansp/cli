45849f8b425760b80c42dbe6b6e391e24d139e55
Rostislav Bobrovsky <rostislav.bobrovsky@gmail.com>
2020-07-09 14:21:52 +0300

    fix: dynamically import transform function

--- compilers/schemaCompiler.js
+++ compilers/schemaCompiler.js
@@ -1,9 +1,10 @@
 import fs from 'fs'
-import path from 'path'
 
-import { transform } from '../compile/transform'
+import { paths } from '../constants'
 
 export async function compileSchema(schemaPath, componentsPath) {
+    const { transform } = await import(paths.mould.byVersionDirectory)
+
     const schema = await fs.promises.readFile(schemaPath, 'utf8')
 
     await fs.promises.writeFile(componentsPath, transform(JSON.parse(schema)))
